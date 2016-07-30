fa44e1f98acc10467e7fc4442f8effab69affdb2
Arief <ashabul.kahfi8@gmail.com>
2016-07-30 14:27:05 +0700

    Added View Holder pattern

--- app/src/main/java/com/swdmnd/sofcapp/DrawerListAdapter.java
+++ app/src/main/java/com/swdmnd/sofcapp/DrawerListAdapter.java
@@ -13,25 +13,37 @@ import android.widget.TextView;
  * A class for populating list view items
  */
 public class DrawerListAdapter extends ArrayAdapter<String> {
-    private final String[] itemname;
-    private final int[] imgid;
+    private final String[] itemName;
+    private final int[] imgId;
     private LayoutInflater layoutInflater;
 
-    public DrawerListAdapter(Activity context, String[] itemname, int[] imgid) {
-        super(context, R.layout.drawer_list_item, itemname);
-        // TODO Auto-generated constructor stub
-        this.itemname=itemname;
-        this.imgid=imgid;
+    static class ViewHolder{
+        TextView titleText;
+        ImageView imageView;
+    }
+
+    public DrawerListAdapter(Activity context, String[] itemName, int[] imgId) {
+        super(context, R.layout.drawer_list_item, itemName);
+
+        this.itemName=itemName;
+        this.imgId=imgId;
         layoutInflater = context.getLayoutInflater();
     }
 
     public View getView(int position,View view,ViewGroup parent) {
-        View rowView=layoutInflater.inflate(R.layout.drawer_list_item, null,true);
-        TextView txtTitle = (TextView) rowView.findViewById(R.id.drawer_list_title);
-        ImageView imageView = (ImageView) rowView.findViewById(R.id.drawer_img);
+        ViewHolder mViewHolder;
+        if(view == null){
+            mViewHolder = new ViewHolder();
+            view = layoutInflater.inflate(R.layout.drawer_list_item, null,true);
+            mViewHolder.titleText = (TextView) view.findViewById(R.id.drawer_list_title);
+            mViewHolder.imageView = (ImageView) view.findViewById(R.id.drawer_img);
+            view.setTag(mViewHolder);
+        } else {
+            mViewHolder = (ViewHolder) view.getTag();
+        }
 
-        txtTitle.setText(itemname[position]);
-        imageView.setImageResource(imgid[position]);
-        return rowView;
+        mViewHolder.titleText.setText(itemName[position]);
+        mViewHolder.imageView.setImageResource(imgId[position]);
+        return view;
     }
 }
